f7652b85e1ad9dbd1d3ea2393fbedfe9984868b2
Brad Bunce <76120543+bradbunce@users.noreply.github.com>
2025-01-12 23:57:01 -0500

    remove jwt-decode

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import jwt_decode from 'jwt-decode';
 
 const AUTH_API_URL = process.env.REACT_APP_AUTH_API;
 const TOKEN_STORAGE_KEY = 'authToken';
