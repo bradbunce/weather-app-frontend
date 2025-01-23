4d20f9e4d95c042d45976a287b146c85a2fe1ec9
Brad Bunce <76120543+bradbunce@users.noreply.github.com>
2025-01-23 14:02:36 -0500

    update to map data

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -33,16 +33,15 @@ export const WeatherCard = React.memo(
         if (message.data) {
           setWeatherState((prev) => ({
             ...prev,
-            data: message.data,
+            data: {
+              temperature: message.data.temp_f,
+              condition: message.data.condition_text,
+              humidity: message.data.humidity,
+              windSpeed: message.data.wind_mph,
+              timestamp: message.data.last_updated
+            },
             loading: false,
             error: "",
-            isConnected: true,
-          }));
-        } else {
-          setWeatherState((prev) => ({
-            ...prev,
-            loading: false,
-            error: "No data available for this location",
             isConnected: true,
           }));
         }
